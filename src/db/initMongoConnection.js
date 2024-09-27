cc0a765d7a108714301ad46f78c3330356d77420
1Rendes <1admin@ukr.net>
2024-09-27 18:26:19 +0200

    rev 1.0

---
+++ src/db/initMongoConnection.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import { env } from '../utils/env.js';
+
+export async function initMongoConnection() {
+  try {
+    const user = env('USER');
+    const pw = env('PASSWORD');
+    const url = env('URL');
+    const db = env('DB');
+    await mongoose.connect(
+      `mongodb+srv://${user}:${pw}@${url}/${db}?retryWrites=true&w=majority`,
+    );
+    console.log('Mongo connection successfully established!');
+  } catch (error) {
+    console.log('Error while setting up mongo connection', error);
+    throw error;
+  }
+}
